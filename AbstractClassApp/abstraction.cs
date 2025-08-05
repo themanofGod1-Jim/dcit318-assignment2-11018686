9ded108e6a46128f4ba7a44e819ff9ae3056a0c3
Isaac-Jim <jimawuahisaac>
2025-08-05 20:28:21 +0000

    new: implemented abstraction for circle and rectangle

---
+++ AbstractClassApp/abstraction.cs
@@ -0,0 +1,57 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace AbstractClassesApp
+{
+    // Abstract class
+    abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape circle = new Circle(5);
+            Shape rectangle = new Rectangle(4, 6);
+
+            Console.WriteLine($"Area of Circle: {circle.GetArea():F2}");
+            Console.WriteLine($"Area of Rectangle: {rectangle.GetArea():F2}");
+        }
+    }
+}
+
