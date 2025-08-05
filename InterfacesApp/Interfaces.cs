8beacca7d4e41c71594e4bd9020c70ac1511bbcb
Isaac-Jim <jimawuahisaac>
2025-08-05 20:36:02 +0000

    new: added interfacesApp

---
+++ InterfacesApp/Interfaces.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace InterfacesApp
+{
+    // Define interface
+    interface IMovable
+    {
+        void Move();
+    }
+
+    // Implement interface in Car
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    // Implement interface in Bicycle
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMovable car = new Car();
+            IMovable bicycle = new Bicycle();
+
+            car.Move();
+            bicycle.Move();
+        }
+    }
+}
+
